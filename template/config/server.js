6de157e5cf9abe90ca3247ee450bdc7a180a0d11
ElegantStack <elegantstack.studio@gmail.com>
2020-12-18 11:55:27 -0800

    remove unwanted config files

--- template/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '1123d5e24e65d2d9134dd66449dfabae'),
-    },
-  },
-});
