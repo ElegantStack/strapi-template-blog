bea04351e22a6dbe5c196b9dab4e4360f3c7eace
ElegantStack <elegantstack.studio@gmail.com>
2020-12-28 06:19:27 -0800

    flatten tags in article collection

--- template/api/article/controllers/article.js
+++ template/api/article/controllers/article.js
@@ -1,16 +1,29 @@
-const { sanitizeEntity } = require('strapi-utils');
+const { sanitizeEntity } = require('strapi-utils')
 
 module.exports = {
-  /**
-   * Retrieve a record.
-   *
-   * @return {Object}
-   */
+	/**
+	 * Retrieve records.
+	 *
+	 * @return {Object}
+	 */
 
-  async findOne(ctx) {
-    const { slug } = ctx.params;
+	async find(ctx) {
+		let entities
+		if (ctx.query._q) {
+			entities = await strapi.services.article.search(ctx.query)
+		} else {
+			entities = await strapi.services.article.find(ctx.query)
+		}
+		return entities.map((entity) => {
+			const article = sanitizeEntity(entity, {
+				model: strapi.models.article,
+			})
 
-    const entity = await strapi.services.article.findOne({ slug });
-    return sanitizeEntity(entity, { model: strapi.models.article });
-  },
-};
+			if (article.tags) {
+				article.tags = article.tags.map((tag) => tag.name)
+			}
+
+			return article
+		})
+	},
+}
