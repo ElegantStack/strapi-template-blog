eb7015ac0988ac31d06e1248d2efef499ec06553
ElegantStack <elegantstack.studio@gmail.com>
2020-12-28 08:52:45 -0800

    add controllers to article and author models

--- template/api/article/controllers/article.js
+++ template/api/article/controllers/article.js
@@ -1,29 +1,57 @@
-const { sanitizeEntity } = require('strapi-utils')
+const { sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
-	/**
-	 * Retrieve records.
-	 *
-	 * @return {Object}
-	 */
+  /**
+   * Retrieve a record.
+   *
+   * @return {Object}
+   */
 
-	async find(ctx) {
-		let entities
-		if (ctx.query._q) {
-			entities = await strapi.services.article.search(ctx.query)
-		} else {
-			entities = await strapi.services.article.find(ctx.query)
-		}
-		return entities.map((entity) => {
-			const article = sanitizeEntity(entity, {
-				model: strapi.models.article,
-			})
+  async findOne(ctx) {
+    const { slug } = ctx.params;
 
-			if (article.tags) {
-				article.tags = article.tags.map((tag) => tag.name)
-			}
+    const entity = await strapi.services.article.findOne({ slug });
+    return sanitizeEntity(entity, { model: strapi.models.article });
+  },
 
-			return article
-		})
-	},
-}
+  /**
+   * Retrieve records.
+   *
+   * @return {Object}
+   */
+
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.article.search(ctx.query);
+    } else {
+      entities = await strapi.services.article.find(ctx.query);
+    }
+
+    return entities.map((entity) => {
+      const article = sanitizeEntity(entity, {
+        model: strapi.models.article,
+      });
+
+      // Flatten tags
+      if (article.tags) {
+        article.tags = article.tags.map(({ value }) => value).filter(Boolean);
+      }
+
+      // Flatten keywords
+      if (article.keywords) {
+        article.keywords = article.keywords
+          .map(({ value }) => value)
+          .filter(Boolean);
+      }
+
+      // Replace body with rawBody for converting via mdx resolver
+      if (article.body) {
+        article.rawBody = article.body;
+        delete article.body;
+      }
+
+      return article;
+    });
+  },
+};
--- template/api/author/controllers/author.js
+++ template/api/author/controllers/author.js
@@ -1,8 +1,49 @@
-'use strict';
+const { sanitizeEntity } = require("strapi-utils");
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Retrieve a record.
+   *
+   * @return {Object}
+   */
 
-module.exports = {};
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const entity = await strapi.services.author.findOne({ slug });
+    return sanitizeEntity(entity, { model: strapi.models.author });
+  },
+
+  /**
+   * Retrieve records.
+   *
+   * @return {Object}
+   */
+
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.author.search(ctx.query);
+    } else {
+      entities = await strapi.services.author.find(ctx.query);
+    }
+
+    return entities.map((entity) => {
+      const author = sanitizeEntity(entity, {
+        model: strapi.models.author,
+      });
+
+      // Flatten social
+      if (author.social) {
+        author.social = author.social.map(({ value }) => value).filter(Boolean);
+      }
+
+      // Flatten skills
+      if (author.skills) {
+        author.skills = author.skills.map(({ value }) => value).filter(Boolean);
+      }
+
+      return author;
+    });
+  },
+};
