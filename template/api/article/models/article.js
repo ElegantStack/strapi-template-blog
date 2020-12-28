3001382802b9d97dda70fdf32fa33739221c1474
ElegantStack <elegantstack.studio@gmail.com>
2020-12-28 12:08:43 -0800

    make catgeory required field for article

--- template/api/article/models/article.js
+++ template/api/article/models/article.js
@@ -1,8 +1,16 @@
-'use strict';
+const _ = require('lodash')
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+	lifecycles: {
+		async beforeCreate(data) {
+			if (_.isNil(data.category)) {
+				throw strapi.errors.badRequest('Error: Category is required!')
+			}
+		},
+	},
+}
